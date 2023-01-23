2045d5596892a6e992fd892929792e04a9988e57
Replit user <>
2023-01-23 15:00:42 +0000

    progress extracting text from pdf, starting to split audio

--- pdf_to_audio.py
+++ pdf_to_audio.py
@@ -2,8 +2,16 @@ import PyPDF2
 import pytesseract
 
 # Use PyPDF2 to open the PDF file and read its contents
-with open('example.pdf', 'rb') as file:
-    pdf = PyPDF2.PdfFileReader(file)
-    pages = []
-    for page in range(pdf.getNumPages()):
-        pages.append(pdf.getPage(page).extractText())
+with open('from-third-world-to-first-by-lee-kuan-yew.pdf', 'rb') as file:
+  pdf = PyPDF2.PdfReader(file)
+  pages = []
+  for page in range(len(pdf.pages)):
+    pages.append(pdf.pages[page].extract_text())
+
+# Concatenate all the pages to get the entire text
+text = '\n'.join(pages)
+
+# Open a new file to write the extracted text
+with open('extracted_text.txt', 'w') as file:
+  file.write(text)
+
